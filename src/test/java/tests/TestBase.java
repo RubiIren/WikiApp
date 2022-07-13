3d0b248c694b75c6cfe2dee670201b0dbfa3c9c8
Irina Rubinshtein <rubinshteiniv@gmail.com>
2022-07-13 16:35:21 +0300

    HW

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.Attach.sessionId;
 import static io.qameta.allure.Allure.step;
@@ -23,19 +23,17 @@ public class TestBase {
     @BeforeEach
     public void startDriver() {
         addListener("AllureSelenide", new AllureSelenide());
-
         open();
+        sleep(50000);
+        back();
     }
 
     @AfterEach
     public void afterEach() {
         String sessionId = sessionId();
-
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
-
-        step("Close driver", Selenide::closeWebDriver);
-
         Attach.video(sessionId);
+        step("Close driver", Selenide::closeWebDriver);
     }
 }

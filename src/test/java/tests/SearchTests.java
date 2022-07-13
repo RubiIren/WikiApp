055517be07ac4ba1cd63c4d1f10a7191e9ed01ff
Irina Rubinshtein <rubinshteiniv@gmail.com>
2022-07-13 15:00:17 +0300

    HW

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -2,13 +2,18 @@ package tests;
 
 import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class SearchTests extends TestBase {
+    @Tag("android")
     @Test
+    @DisplayName("Проверка выдачи")
     void searchTest() {
         back();
         step("Type search", () -> {
@@ -18,5 +23,44 @@ public class SearchTests extends TestBase {
         step("Verify content found", () ->
                 $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(CollectionCondition.sizeGreaterThan(0)));
+    }
+
+    @Tag("android")
+    @Test
+    @DisplayName("Проверка наличия description")
+    void descriptionTest() {
+        step("Type search", () -> {
+            back();
+            step("Type search", () -> {
+                $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
+                $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("cat");
+            });
+
+            step("Go to article", () -> {
+                $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_description"))
+                        .shouldHave(text("Domesticated felid species"
+                        ));
+            });
+        });
+    }
+
+    @Tag("android")
+    @Test
+    @DisplayName("Проверка перехода на статью")
+    void openArticalTest() {
+        step("Type search", () -> {
+            back();
+            step("Type search", () -> {
+                $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
+                $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("cat");
+            });
+
+            step("Go to article", () -> {
+                $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
+                $(AppiumBy.className("android.view.View")).click();
+                $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Cat"));
+            });
+        });
+
     }
 }
